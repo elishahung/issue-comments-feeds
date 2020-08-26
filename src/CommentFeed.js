c5bb5bc531762c3259652d1a73daabad9329c7aa
Elisha Hung <mrhchief@gmail.com>
2020-08-27 02:14:07 +0800

    make style more like github. add comment feed's meta.

--- src/CommentFeed.js
+++ src/CommentFeed.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown'
+import TimeAgo from 'timeago-react';
 
 
 const CommentFeed = props => {
   const { comment } = props;
   return (
     <div className="comment-feed">
-      <ReactMarkdown source={comment.body} />
+      <div className='meta'>
+        <img src={comment.user.avatar_url} alt='avatar' />
+        <a href={comment.user.html_url} target='_blank' className={'username'}>{comment.user.login}</a>
+        <span>&nbsp;commented&nbsp;</span>
+        <TimeAgo
+          datetime={comment.updated_at}
+        />
+      </div>
+      <ReactMarkdown className='markdown' source={comment.body} />
     </div>
   )
 };
